35ee0567a38701585e809162259039d2700f8a1f
Roland SAIKALI <roland@saikali.fr>
2014-02-07 16:10:02 +0100

    Form errors

--- app/views.py
+++ app/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
-from django import forms
 from app.forms import ConfigInput
-from django.http import HttpResponse
+from django import forms
+from django.core.exceptions import ValidationError
+from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
     """ EZTLS Homepage"""
 
     return render(request, 'home.html')
+
 
 def config_result(request):
     """ EZTLS Generated configuration """
@@ -21,10 +23,10 @@ def config_result(request):
             win_xp = form.cleaned_data['win_xp']
             force_https = form.cleaned_data['force_https']
             webserver = form.cleaned_data['webservers_choices']
+            return render(request, "config_base.html", {'fqdn': fqdn, 'ie6': ie6, 'win_xp': win_xp, 'force_https': force_https, 'webserver': webserver})
         else:
-            form = ConfigInput()
-        return render(request, "config_base.html", {'fqdn': fqdn, 'ie6': ie6, 'win_xp': win_xp, 'force_https': force_https, 'webserver': webserver })
+            return render(request, 'config_input.html', {'form': form})
     else:
         form = ConfigInput()
-        return render(request, 'config_input.html', {'form': form })
+        return render(request, 'config_input.html', {'form': form})
 
